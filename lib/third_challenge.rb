42cff934d4415975dd1359c691dafcbae3c274a1
rosa lee <rosalee0116@gmail.com>
2019-02-16 20:29:56 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,7 +11,7 @@ def third_challenge
       :hero => {
         #your key/value pairs here
         name: "Romeo",
-        age: "15"
+        age: "15",
         status: "alive"
         },
       :hero_friends => [name: "Bemvolio", age: "17", attitude: "worried"]
